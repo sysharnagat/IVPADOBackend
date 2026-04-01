fd1c25ae45f16c76d8a377de74083852d9a399c8
hchowdhry2 <hchowdhry@ivp.in>
2026-04-01 14:55:39 +0530

    scaffolded the task tables

---
+++ SRMDevOps/Models/IvpTaskIteration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SRMDevOps.Models;
+
+public partial class IvpTaskIteration
+{
+    public int TaskId { get; set; }
+
+    public DateTime AssignedDate { get; set; }
+
+    public string? IterationPath { get; set; }
+}
